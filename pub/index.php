11f0f8b9bf4981b7b161e82fb70d428abc1f9c3c
Peprykos <pathan99@wp.pl>
2023-04-28 12:28:13 +0200

    initial commit

---
+++ pub/index.php
@@ -0,0 +1,96 @@
+<?php
+require("./../src/config.php");
+session_start();
+use Steampixel\Route;
+Route::add('/', function(){
+    global $twig;
+    $posts = Post::getPage();
+    $t = array("posts" => $posts);
+    if(isset($_SESSION['user'])) {
+        $t['user'] = $_SESSION['user'];
+    } 
+    $twig->display("index.html", $t);
+});
+Route::add('/upload', function() {
+    global $twig;
+    if(User::isAuth()) {
+        $t['user'] = $_SESSION['user'];
+        $twig->display("upload.html", $t);
+    } else {
+        http_response_code(403);
+    }
+});
+Route::add('/upload', function() {
+    global $twig;
+    $tempFileName = $_FILES['uploadedFile']['tmp_name'];
+    $title = $_POST['title'];
+    Post::upload($tempFileName, $title, $_POST['userId']);
+    header("Location: /projekt-cms/pub");
+    die();
+}, 'post');
+Route::add('/register', function() {
+    global $twig;
+    $twigData = array("pageTitle" => "Zarejestruj użytkownika");
+    $twig->display("register.html", $twigData);
+});
+Route::add('/register', function(){
+    global $twig;
+    if(isset($_POST['submit'])) {
+        User::register($_POST['email'], $_POST['password']);
+        header("Location: /projekt-cms/pub");
+    }
+}, 'post');
+Route::add('/login', function(){
+    global $twig;
+    $twigData = array("pageTitle" => "Zaloguj użytkownika");
+    $twig->display("login.html", $twigData);
+});
+Route::add('/login', function() {
+    global $twig;
+    if(isset($_POST['submit'])) {
+        if (User::login($_POST['email'], $_POST['password'])) {
+            header("Location: /projekt-cms/pub");
+        } else {
+            $t = array("message" => "Nieprawidłowy użytkownik lub hasło");
+            $twig->display("login.html", $t);
+        }
+    }
+}, 'post');
+Route::add('/admin', function() {
+    global $twig;
+    if(User::isAuth()) {
+        $t = array( "postList" => Post::getPage(1, 100));
+        $twig->display("admin.html", $t);
+
+    } else {
+        http_response_code(403);
+    }
+});
+Route::add('/admin/remove/([0-9]*)', function($id) {
+    if(User::isAuth()) {
+        Post::remove($id);
+        header("Location: /projekt-cms/admin");
+    } else {
+        http_response_code(403);
+    }
+});
+Route::add('/like/([0-9]*)', function($post_id) {
+    if(!User::isAuth()) {
+        http_response_code(403);
+    } else {
+        $user_id = $_SESSION['user']->getId();
+        $like = new Likes($post_id, $user_id, 1);
+        header("Location: /projekt-cms/pub");
+    }
+});
+Route::add('/dislike/([0-9]*)', function($post_id) {
+    if(!User::isAuth()) {
+        http_response_code(403);
+    } else {
+        $user_id = $_SESSION['user']->getId();
+        $like = new Likes($post_id, $user_id, -1);
+        header("Location: /projekt-cms/pub");
+    }
+});
+Route::run('/projekt-cms/pub');
+?>
---
+++ pub/sandbox/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sandbox - index</title>
+</head>
+<body>
+
+    <form actions="" method="post" enctype="multipart/form-data">
+        <label for="uploadedFileInput">
+                Wybierz plik do wgrania na serwer:
+        </label>
+        <input type="file" name="uploadedFile" id="uploadedFileInput">
+        <input type="submit" value="Wyślij plik" name="submit">
+    </form>
+<?php 
+    $db = new mysqli('localhost', 'root', '', 'memy');
+    if(isset($_POST['submit']))
+    {
+        $fileName = $_FILES['uploadedFile']['name'];
+        $targetDir = "img/";
+        $imageInfo = getimagesize($_FILES["uploadedFile"]["tmp_name"]);
+        if(!is_array($imageInfo)) {
+            die("Nieprawidłowy format obrazu!");
+        }
+        $imgString = file_get_contents($_FILES["uploadedFile"]["tmp_name"]);
+        $gdImage = imagecreatefromstring($imgString);
+        $targetExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+        $targetExtension = strtolower($targetExtension);
+        $targetFileName = $fileName . hrtime(true);
+        $targetFileName = hash("sha256", $targetFileName);
+        $targetUrl = $targetDir . $targetFileName . "." . $targetExtension;      
+        if(file_exists($targetUrl))
+        {
+            die("Plik o tej nazwie już istnieje");
+        }
+        $targetUrl = $targetDir . $targetFileName . ".webp";  
+        imagewebp($gdImage, $targetUrl);
+        $fileName = $targetFileName . ".webp";
+        $dateTime = DATE("Y-m-d H:i:s");
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $sql = "INSERT INTO post (timestamp, filename, ip)
+            VALUES ('$dateTime', '$fileName', '$ip')";
+        $db->query($sql);
+        $db->close();
+    }
+?>
+</body>
+</html>
